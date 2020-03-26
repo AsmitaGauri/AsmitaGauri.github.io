56b035a10d7e1052c2a9f340c99316bbc044b8c3
asmita gauri <55022713+AsmitaGauri@users.noreply.github.com>
2020-03-26 14:43:54 +0530

    Delete port.js

--- port.js
+++
@@ -1,7 +0,0 @@
-window.onscroll = function() {myFunction()};
-
-function myFunction() {
-  if (document.body.scrollTop > 600 || document.documentElement.scrollTop > 600) {
-    document.getElementById("display").className = "fadein";
-  }
-}
